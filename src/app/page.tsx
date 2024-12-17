8778d816aac0ff07e94a96ab21604b16d76734e9
ratihjulistina <ratihjulistina@gmail.com>
2024-12-17 10:16:43 +0700

    Remove unused import in Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,9 @@ import About from "@/components/layout/About";
 import { Articles } from "@/components/layout/Articles";
 import { Catagories } from "@/components/layout/Catagories";
 import { Homepage } from "@/components/layout/Homepage";
-// import About from "@/components/layout/About";
-// import Hero from "@/components/layout/Hero";
 import Navbar from "@/components/layout/Navbar";
-// import Skills from "@/components/layout/Skills";
-// import Portfolio from "@/components/layout/Portfolio";
 import { GlobalStateContext } from "@/context/GlobalStateContext";
 import { useGlobal } from "@/hooks/global.customhooks";
-// import Experience from "@/components/layout/Experience";
-// import Testimonial from "@/components/layout/Testimonial";
-// import Contact from "@/components/layout/Contact";
-// import Modal from "@/components/layout/Modal";
 
 export default function Home() {
   const {
